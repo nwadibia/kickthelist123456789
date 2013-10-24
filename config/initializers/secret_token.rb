b998ebb82904867b8cb1cb9d237f24d5b80f05e4
Emeka <enwadibia@gmail.com>
2013-10-23 21:03:17 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kickthelist::Application.config.secret_key_base = '91b01b2525a0ef43676d87eae4d7f390ca5935fb2a2d6e14f3db48f23df78237fc5891d23ea78725ddbc2541639ac5cf12fd56fcef205fea6b0c66c3985ca589'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kickthelist::Application.config.session_store :cookie_store, key: '_kickthelist_session'
