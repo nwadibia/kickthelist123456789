7928359a705199afa660cfb29b99d13fede54067
Emeka <enwadibia@gmail.com>
2013-11-03 22:09:26 -0800

    added scaffold for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,94 +1,42 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-<<<<<<< HEAD
-  # GET /items
-  # GET /items.json
-=======
->>>>>>> 1b7f44ecaec92d4eab21ed476e7f548db7132621
   def index
     @items = Item.all
   end
 
-<<<<<<< HEAD
-  # GET /items/1
-  # GET /items/1.json
   def show
   end
 
-  # GET /items/new
-=======
-  def show
-  end
-
->>>>>>> 1b7f44ecaec92d4eab21ed476e7f548db7132621
   def new
     @item = Item.new
   end
 
-<<<<<<< HEAD
-  # GET /items/1/edit
   def edit
   end
 
-  # POST /items
-  # POST /items.json
-=======
-  def edit
-  end
-
->>>>>>> 1b7f44ecaec92d4eab21ed476e7f548db7132621
   def create
     @item = Item.new(item_params)
-
-    respond_to do |format|
-      if @item.save
-<<<<<<< HEAD
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
-=======
-        format.html { redirect_to @item, notice: 'Succesfully listed a new item, youre one step closer to awesome!' }
->>>>>>> 1b7f44ecaec92d4eab21ed476e7f548db7132621
-        format.json { render action: 'show', status: :created, location: @item }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.save
+      redirect_to @item, notice: 'Item was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /items/1
-  # PATCH/PUT /items/1.json
   def update
-    respond_to do |format|
-      if @item.update(item_params)
-<<<<<<< HEAD
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
-=======
-        format.html { redirect_to @item, notice: 'Successfully updated.' }
->>>>>>> 1b7f44ecaec92d4eab21ed476e7f548db7132621
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Item was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
-  # DELETE /items/1
-  # DELETE /items/1.json
   def destroy
     @item.destroy
-    respond_to do |format|
-      format.html { redirect_to items_url }
-      format.json { head :no_content }
-    end
+    redirect_to items_url
   end
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 1b7f44ecaec92d4eab21ed476e7f548db7132621
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_item
